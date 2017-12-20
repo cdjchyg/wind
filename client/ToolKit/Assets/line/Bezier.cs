9bad9e6f7bfa2a3f76864f7427ad1843aeaafb80
hyg <hygeml@qq.com>
2017-12-20 08:38:37 +0800

    贝塞尔曲线实现

---
+++ client/ToolKit/Assets/line/Bezier.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * //http://gad.qq.com/article/detail/36557
+ * 
+ * B（t）=（1-t）P0 + t P1
+ * 
+ * B（t）=（1-t）（（1-t）P0 + t P1）+ t（（1-t）P1 + t P2）  ->  B（t）=（1-t）2 P0 + 2（1-t）t P1 + t2 P2
+ * 导数:  B'（t）= 2（1-t）（P1-P0）+ 2t（P2-P1）  (方向, 解释为沿着曲线移动的速度)
+ * 
+ * 
+ */
+
+public class Bezier{
+
+    public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, float t)
+    {
+        //return Vector3.Lerp(p0, p2, t);
+        //return Vector3.Lerp(Vector3.Lerp(p0, p1, t), Vector3.Lerp(p1, p2, t), t);
+
+        t = Mathf.Clamp01(t);
+        float oneMinusT = 1f - t;
+        return oneMinusT * oneMinusT * p0
+            + 2f * oneMinusT * t * p1
+            + t * t * p2;
+    }
+
+    // 方向
+    public static Vector3 GetFirstDerivative(Vector3 p0, Vector3 p1, Vector3 p2, float t)
+    {
+        return
+            2f * (1f - t) * (p1 - p0) +
+            2f * t * (p2 - p1);
+    }
+
+    public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+    {
+        t = Mathf.Clamp01(t);
+        float oneMinusT = 1f - t;
+        return
+            oneMinusT * oneMinusT * oneMinusT * p0 +
+            3f * oneMinusT * oneMinusT * t * p1 +
+            3f * oneMinusT * t * t * p2 +
+            t * t * t * p3;
+    }
+
+    public static Vector3 GetFirstDerivative(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+    {
+        t = Mathf.Clamp01(t);
+        float oneMinusT = 1f - t;
+        return
+            3f * oneMinusT * oneMinusT * (p1 - p0) +
+            6f * oneMinusT * t * (p2 - p1) +
+            3f * t * t * (p3 - p2);
+    }
+}
---
+++ client/ToolKit/Assets/line/test/SplineWalker.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplineWalker : MonoBehaviour {
+
+    public BezierSpline spline;
+
+    public float duration;
+
+    private float progress;
+
+    public enum SplineWalkerMode
+    {
+        Once,
+        Loop,
+        PingPong
+    }
+
+    public SplineWalkerMode mode;
+    private bool goingForward = true;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (goingForward)
+        {
+            progress += Time.deltaTime / duration;
+            if (progress > 1f)
+            {
+                if (mode == SplineWalkerMode.Once)
+                {
+                    progress = 1f;
+                }
+                else if (mode == SplineWalkerMode.Loop)
+                {
+                    progress -= 1f;
+                }
+                else
+                {
+                    progress = 2f - progress;
+                    goingForward = false;
+                }
+            }
+        }
+        else
+        {
+            progress -= Time.deltaTime / duration;
+            if (progress < 0f)
+            {
+                progress = -progress;
+                goingForward = true;
+            }
+        }
+
+
+        Vector3 position = spline.GetPoint(progress);
+        transform.localPosition = position;
+
+        transform.LookAt(position + spline.GetDirection(progress));
+	}
+}
