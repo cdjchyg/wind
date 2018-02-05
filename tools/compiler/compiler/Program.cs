9136f710ccd8b1b71101ce407f9d90df73690409
hyg <hygeml@qq.com>
2018-02-05 12:11:41 +0800

    add compiler proj

---
+++ tools/compiler/compiler/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a compiler for learn!");
+        }
+    }
+}
---
+++ tools/compiler/readme.txt
@@ -0,0 +1,2 @@
+http://www.cnblogs.com/Ninputer/archive/2011/06/07/2074632.html
+
