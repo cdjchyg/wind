ebe261e739787f928c73c4853178cb01fde740c5
hyg <heyueguo@digisky.com>
2018-03-02 20:09:01 +0800

    up

--- tools/cpp_embed_mono/cpp_embed_mono/main.cpp
+++ tools/cpp_embed_mono/cpp_embed_mono/main.cpp
@@ -1,4 +1,7 @@
 
+//http://www.mono-project.com/docs/advanced/embedding/
+
+
 #include <iostream>
 
 #include <mono/jit/jit.h>
@@ -7,6 +10,11 @@
 
 using namespace std;
 
+static MonoString* gimme()
+{
+	return mono_string_new(mono_domain_get(), "c++ return string");
+}
+
 int main(int argc, char* argv)
 {
 	cout << "c++ embedding mono test\n";
@@ -14,6 +22,8 @@ int main(int argc, char* argv)
 	//mono_set_assemblies_path("");
 
 	MonoDomain* domain = mono_jit_init("script_domain");
+
+	mono_add_internal_call("MonoCsharp.MainTest::gimme", gimme);
 	
 	MonoAssembly* assembly = mono_domain_assembly_open(domain, "G:\\wind\\tools\\cpp_embed_mono\\script\\Program.dll");
 	MonoImage* image = mono_assembly_get_image(assembly);
@@ -26,7 +36,7 @@ int main(int argc, char* argv)
 
 	mono_runtime_invoke(method, NULL, NULL, NULL);
 
-	mono_runtime_invoke(method, NULL, NULL, NULL);
+	//mono_runtime_invoke(method, NULL, NULL, NULL);
 
 	mono_jit_cleanup(domain);
 
--- tools/cpp_embed_mono/script/Program.cs
+++ tools/cpp_embed_mono/script/Program.cs
@@ -3,26 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 
 namespace MonoCsharp
 {
     public static class MainTest
     {
+		[MethodImplAttribute(MethodImplOptions.InternalCall)]
+		extern static string gimme();
+		
         static int mVar = 1;
 
         static void Main()
         {
-			try{
-            System.Console.WriteLine("你好,世界 " + mVar.ToString());
+            System.Console.WriteLine("你好,世界 " + mVar.ToString() + " " + gimme() );
 
             //mVar = 1/mVar;
 
             //++mVar;
-            throw new ArgumentException("test");
-			}
-			catch(ArgumentException e){
-				Console.WriteLine(e.ToString());
-			}
+            
         }
     }
 }
