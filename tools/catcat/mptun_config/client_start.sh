11b7174d2f5745a6170565e4d1f30992903f3c10
hyg <heyueguo@digisky.com>
2018-07-11 16:11:23 +0800

    up

--- tools/catcat/mptun_config/client_start.sh
+++ tools/catcat/mptun_config/client_start.sh
@@ -2,8 +2,8 @@
 killall mptun
 ./mptun -i tun0 -p 2200 -v 192.168.0.1 -t 192.168.0.2 -r 104.225.145.30 -k hyg2015
 sleep 3 
-route add -net 0.0.0.0/0 gw 192.168.0.1
 route add -host 104.225.145.30 gw 10.0.2.2
+route add -net 0.0.0.0/0 gw 192.168.0.1
 echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -A FORWARD -i tun+ -j ACCEPT
 iptables -A INPUT -i tun+ -j ACCEPT
